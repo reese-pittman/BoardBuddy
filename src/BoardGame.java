7a3df720ffe885e31b1d54d5113f907f67b3c38a
whymaison <mwn319@proton.me>
2026-01-28 14:13:59 -0600

    Barebones BoardGame class and Untested but complete InputXml class.

---
+++ src/BoardGame.java
@@ -0,0 +1,68 @@
+public class BoardGame {
+
+    /**
+     * 
+     * @param sId
+     */
+    public void setId(String sId) {
+        id = sId;
+    }
+    /**
+     * 
+     * @param sName
+     */
+    public void setName(String sName) {
+        name = sName;
+    }
+    /**
+     * 
+     * @param sMin
+     */
+    public void setMin(int sMin) {
+        minPlayers = sMin;
+    }
+    /**
+     * 
+     * @param sMax
+     */
+    public void setMax(int sMax) {
+        maxPlayers = sMax;
+    }
+    /**
+     * 
+     * @param sPlay
+     */
+    public void setPlay(int sPlay) {
+        playTime = sPlay;
+    }
+    /**
+     * 
+     * @param sYear
+     */
+    public void setYear(int sYear) {
+        year = sYear;
+    }
+    /**
+     * 
+     * @param sImage
+     */
+    public void setImage(String sImage) {
+        name = sImage;
+    }
+    /**
+     * 
+     * @param sThumbnail
+     */
+    public void setThumbnail(String sThumbnail) {
+        name = sThumbnail;
+    }
+
+    private String id;
+    private String name;
+    private int minPlayers;
+    private int maxPlayers;
+    private int playTime;
+    private int year;
+    private String image;
+    private String thumbnail;
+}
